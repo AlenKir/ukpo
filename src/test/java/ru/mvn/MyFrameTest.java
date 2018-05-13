09921bb573c360d67c9b90058b565e7f902550ef
AlenMuseJ <alen.muse@yandex.ru>
2018-05-13 15:57:29 +0300

    jdk8

--- src/test/java/ru/mvn/MyFrameTest.java
+++ src/test/java/ru/mvn/MyFrameTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 
 public class MyFrameTest {
-  /*  
+ 
  private FrameFixture window;
     
     @Before
@@ -102,6 +102,6 @@ public class MyFrameTest {
   public void tearDown() {
     window.cleanUp();
   }
- */   
+
 }
 
